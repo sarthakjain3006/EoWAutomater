d651a471fca38bfc68d263c6a72bb71cc6046d0a
sarthakjai3006 <sarthakjain3006@gmail.com>
2024-12-15 18:14:06 -0500

    updating template with detailed descriptions and defaults

--- template.py
+++ template.py
@@ -1,26 +1,70 @@
-from pydantic import BaseModel
-from typing import List, Optional
+from pydantic import BaseModel, Field
+
+
+class Progress(BaseModel):
+    headline: str = Field(
+        default="summarizing headline",
+        description="A minimal headline that encapsulates the changes that follow",
+    )
+    changes: list[str] = Field(
+        default=["change from commits"],
+        description="all changes from commits in a list",
+    )
+
+
+class Roadblock(BaseModel):
+    headline: str = Field(
+        default="summarizing headline",
+        description="A minimal headline that encapsulates the roadblocks",
+    )
+    changes: list[str] = Field(
+        default=["change from commits"],
+        description="all possible roadblocks explained in detail",
+    )
+
 
 class WeeklyUpdate(BaseModel):
-    summary: List[str] = ["",""]  # List of points under "Summary"
-    progress: List[str] = ["",""]  # List of points under "Progress"
-    roadblocks: List[str] = ["",""]  # List of points under "Roadblocks"
-    next_week_work: List[str] = ["",""]  # List of points under "Next Week's Work"
-    next_week_work_location: List[str] = ["",""]  # List of work locations for next week
+    summary: str = Field(
+        default="The focus of this week was",
+        description="A concise summary for all the progress in the week",
+    )
+    progress: list[Progress] = Field(
+        default=[Progress()],
+        description="A list of headlines and the respective changes explained in detail from the commits",
+    )  # List of points under "Progress"
+    roadblocks: list[Roadblock] = Field(
+        default=[Roadblock()],
+        description="A list of headlines and the respective roadblocks",
+    )
+    next_week_work: list[str] = Field(
+        default=["Possible work generating from this weeks commits"],
+        description="This can be a gathered from the changes in the commits, and should usually contain possible optimization, security or testing changes",
+    )
 
-raw_template =  """
+
+raw_template = """
 
 ### 1. Summary  
--  
+-  This week's focus was <-- insert summary --> 
 
 ### 2. Progress  
--  
+
+-  Title 1
+    - change 1
+    - change 2
 
 ### 3. Roadblocks  
--  
 
-### 4. Next Week's Work  
--  
+- Title 1
+    - roadblock 1
+    - roadblock 2
 
-### 5. Work Location (Next Week)  
--  """
+### 4. Next Week's Work 
+
+- Title 1
+    - next steps for Progress Title 1
+    - next steps for Progress Title 2
+
+### 5. Work Location
+< -- location if provided in input else "In person all week"
+"""
